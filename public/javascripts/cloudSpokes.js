a571730825ac57bbf9fd0fc60faff88bdcdd99ed
Jeff Douglas <jeff@jeffdouglas.com>
2012-07-05 14:46:49 -0400

    removed

--- public/javascripts/cloudSpokes.js
+++
@@ -1,8 +0,0 @@
-var socket = io.connect();
-
-socket.emit('signingInfo', $('#userId').val(), $('#userName').val(), $('#docId').val(), $('#email').val()); 
-
-socket.on('recipientUrl', function(data) {
-	$('#container h2').remove();
-    $('#container iframe').attr('src', data);
-})
