07d8ed9a0ea19fc690752235d06b27c693aec67f
Jeff Douglas <jeff@jeffdouglas.com>
2012-07-10 14:16:01 -0400

    commented out polling

--- app.js
+++ app.js
@@ -73,11 +73,11 @@ var users = {};
 var connections ={};
 
 // assuming io is the Socket.IO server object
-cloudSpokesIO.configure(function () { 
-  cloudSpokesIO.set("transports", ["xhr-polling"]); 
-  cloudSpokesIO.set("polling duration", 5); 
-  cloudSpokesIO.set("close timeout", 40);  
-});
+// cloudSpokesIO.configure(function () { 
+//   cloudSpokesIO.set("transports", ["xhr-polling"]); 
+//   cloudSpokesIO.set("polling duration", 5); 
+//   cloudSpokesIO.set("close timeout", 40);  
+// });
 
 cloudSpokesIO.set ('authorization', function (data, accept) {
  var cookies = connect.utils.parseSignedCookie (data.headers.cookie)
