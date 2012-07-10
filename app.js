c88d4c41e0b16beaf87ffb3f6ebb1464e4d05a4f
Jeff Douglas <jeff@jeffdouglas.com>
2012-07-09 21:26:14 -0400

    tweak

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ var connections ={};
 // assuming io is the Socket.IO server object
 cloudSpokesIO.configure(function () { 
   cloudSpokesIO.set("transports", ["xhr-polling"]); 
-  cloudSpokesIO.set("polling duration", 10); 
+  cloudSpokesIO.set("polling duration", 30); 
   cloudSpokesIO.set("heartbeat interval", 20);
   cloudSpokesIO.set("heartbeat timeout", 60);  
 });
